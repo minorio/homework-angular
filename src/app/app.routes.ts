b06f8db8ea16172cad47631703f7a836ac300ed0
minorio <mbajduk@mail.ru>
2025-12-25 15:50:15 +0300

    new branch Directive

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { DirectivesPracticeComponent } from './directives-practice/directives-practice.component';
+
+export const routes: Routes = [
+  { path: '', component: DirectivesPracticeComponent },
+  { path: 'directive', component: DirectivesPracticeComponent },
+];
+
---
+++ src/app/directives-practice/directives-practice.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'directives-practice',
+  templateUrl: 'directives-practice.component.html',
+  styleUrls: ['./directives-practice.component.scss'],
+  standalone: true,
+  imports: [],
+})
+
+export class DirectivesPracticeComponent{
+
+}
