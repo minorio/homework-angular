22ab746f28491080a21889966cc32448959b8f64
minorio <mbajduk@mail.ru>
2026-02-03 14:46:26 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { Task1Component } from './task1-input-output/task1/task1.component';
-import { Task2Component } from './task2-viewсhild-contentchild/task2/task2.component';
-import { Task3Component } from './task3-hooks/task3.component';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: Task1Component },
-  { path: 'task1', component: Task1Component },
-  { path: 'task2', component: Task2Component },
-  { path: 'task3', component: Task3Component },
+  { path: '', redirectTo: '/peoples', pathMatch: 'full'},
+  { path: 'peoples', component: PeopleListComponent},
+  { path: '**', component: NotFoundComponent},
+
 ];
 
---
+++ src/app/components/people-list/people-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.scss']
+})
+export class PeopleListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
