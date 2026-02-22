dea968a6f9361d28a921ed928a2a747800174d2b
minorio <mbajduk@mail.ru>
2026-02-22 03:01:50 +0300

    Formatting code

--- src/app/services/demon-slayer.service.ts
+++ src/app/services/demon-slayer.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DemonSlayerService {
   constructor(private http: HttpClient) {}
-  private readonly demonSlayerUrl = '/api/v1/characters'
+  private readonly demonSlayerUrl = '/api/v1/characters';
   getCharacters(page?: number, limit: number = 20) {
-        let params: any = { limit: limit.toString() };
+    let params: any = { limit: limit.toString() };
     if (page !== undefined) {
-      params.page = (page + 1).toString(); 
+      params.page = (page + 1).toString();
     }
-    return this.http.get<any>(this.demonSlayerUrl, { params })
-     .pipe(map((res) => ({
+    return this.http.get<any>(this.demonSlayerUrl, { params }).pipe(
+      map((res) => ({
         characters: res.content,
-        pagination: res.pagination
-      })));
+        pagination: res.pagination,
+      })),
+    );
   }
 }
--- src/app/services/naruto.service.ts
+++ src/app/services/naruto.service.ts
@@ -7,19 +7,13 @@ import { NarutoCharactersResponse } from '../models/naruto.model';
 export class NarutoService {
   constructor(private http: HttpClient) {}
   private readonly narutoUrl = 'https://dattebayo-api.onrender.com/characters';
-  // getCharacters() {
-  //   return this.http.get<any>(this.narutoUrl).pipe(map((res) => res.characters));
-  // }
-    getCharacters(page: number = 0, limit: number = 20) {
-    // let params: any = {
-    //   page: page.toString(),
-    //   limit: limit.toString()
-    // };
-            let params: any = { limit: limit.toString() };
+  getCharacters(page: number = 0, limit: number = 20) {
+    let params: any = { limit: limit.toString() };
     if (page !== undefined) {
-      params.page = (page + 1).toString(); 
+      params.page = (page + 1).toString();
     }
-    return this.http.get<NarutoCharactersResponse>(this.narutoUrl, { params })
+    return this.http
+      .get<NarutoCharactersResponse>(this.narutoUrl, { params })
       .pipe(map((res) => res.characters));
   }
 }
